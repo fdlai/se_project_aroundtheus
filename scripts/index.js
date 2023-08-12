ed02ec9a67ae0ccfceca8cfb9390efd4eabfe10a
Fred <fdlai@yahoo.com>
2023-08-11 22:30:14 -0400

    add index.js and create array of cards

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const initialCards = [
+  { name: "El Capitan", link: "https://unsplash.com/photos/ndN00KmbJ1c" },
+  {
+    name: "Lake Atitl\u00E1n",
+    link: "https://unsplash.com/photos/K2s_YE031CA",
+  },
+  { name: "Banff, Canada", link: "https://unsplash.com/photos/pp_oXEb2H48" },
+  {
+    name: "Ventura, United States",
+    link: "https://unsplash.com/photos/FO7bKvgETgQ",
+  },
+  {
+    name: "Waimea State Recreation Pier",
+    link: "https://unsplash.com/photos/fsJB3KT2rj8",
+  },
+  {
+    name: "Wanaka, New Zealand",
+    link: "https://unsplash.com/photos/St08jKkPVHw",
+  },
+];
